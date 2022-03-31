7339369115b92b947faf49db2f80312f35c8d8df
Mirjam Wolf <mwolf14@cnm.edu>
2022-03-31 16:12:33 -0600

    Adding .insertAdjacentHTML

--- script.js
+++ script.js
@@ -14,9 +14,23 @@ function handleItemForm(event, formRef) {
     if(event.preventDefault) {
        event.preventDefault();
     }
-
-    console.log("Event: ", event);
+    //console.log("Event: ", event);
+    let itemHtml = addItemToShoppingList();
+    let itemListRef = document.getElementById("shopping-list");
+    itemListRef.insertAdjacentHTML("afterend", itemHtml);
 
     return false;
 }
 
+function addItemToShoppingList() {
+    let itemName = document.getElementById("item-name");
+    let itemAmount = document.getElementById("item-amount");
+
+}
+
+function createListItemHtml() {
+    return `<li>
+                Item Name - Amount
+             </li>   
+    `;
+}

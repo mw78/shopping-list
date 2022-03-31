691335a6c3cf6fdf181c2142296310f6a9db049c
Mirjam Wolf <mwolf14@cnm.edu>
2022-03-31 16:26:49 -0600

    corrections

--- script.js
+++ script.js
@@ -24,15 +24,15 @@ function addItemToShoppingList() {
     let itemName = document.getElementById("item-name");
     let itemAmount = document.getElementById("item-amount");
 
-    let itemHtml = createListItemHtml();
+    let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
     console.log("Item HTML: ", itemHtml);
     let itemListRef = document.getElementById("shopping-list");
     itemListRef.insertAdjacentHTML("afterend", itemHtml);
 }
 
-function createListItemHtml() {
+function createListItemHtml(itemName, itemAmount) {
     return `<li>
-                Item Name - Amount
-             </li>   
-    `;
+                ${itemName} - ${itemAmount}
+             </li>`;
 }
+

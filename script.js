16fc858cdd2d887acc955cbced02d0ddb270cc52
Mirjam Wolf <mwolf14@cnm.edu>
2022-03-31 16:00:41 -0600

    Added script.js file

--- script.js
+++ script.js
@@ -4,4 +4,19 @@ window.onload = function () {
 
 function initShoppingList() {
     let form = document.getElementById("item-form");
+
+    form.addEventListener("submit", (event) => {
+         handleItemForm(event, form);
+    });
 }
+
+function handleItemForm(event, formRef) {
+    if(event.preventDefault) {
+       event.preventDefault();
+    }
+
+    console.log("Event: ", event);
+
+    return false;
+}
+
